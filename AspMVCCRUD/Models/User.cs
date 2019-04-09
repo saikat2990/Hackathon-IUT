5155e6940c244367c074d8f24a43b8c2c62124e2
saify2j <bsse0924@iit.du.ac.bd>
2019-04-09 13:32:07 +0600

    first commit

---
+++ AspMVCCRUD/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspMVCCRUD.Models
+{
+    public class User
+    {
+        [Key]
+        public int userId { get; set; }
+
+
+        [Column(TypeName = "nvarchar(250)")]
+
+        [Required]
+        [DisplayName("Username")]
+        public string username { get; set; }
+
+
+        [Column(TypeName = "varchar(100)")]
+        [Required]
+        [DisplayName("Email")]
+        public string email { get; set; }
+
+
+        [Column(TypeName = "varchar(100)")]
+        [Required]
+        [DisplayName("Password")]
+        public string password { get; set; }
+       
+    }
+}
